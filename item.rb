bf70740d99a801341f6bd76f02443226d6d4ce75
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-05-31 10:07:33 +0100

    Update item class

--- item.rb
+++ item.rb
@@ -1,19 +1,35 @@
 require 'date'
 
 class Item
-  attr_accessor :genre, :author, :source, :label
+  attr_accessor :published_date, :author
+  attr_reader :id, :genre, :label
 
-  def initialize(published_date)
-    @id = Random.id(1..1000)
-    @published_date = Date.now(published_date)
-    @achieved = false
+  def initialize(publish_date)
+    @id = rand(1..1000)
+    @published_date = Date.parse(publish_date)
+    @archived = false
   end
 
-  def can_be_archived?
-    (Time.now.year - @published_date.year) > 10
+  def add_genre(genre)
+    @genre = genre
+    genre.items << self
+  end
+
+  def add_author(author)
+    self.author = (author)
+  end
+
+  def add_label(label)
+    @label = label
+    label.items << self unless label.items.include?(self)
   end
 
   def move_to_archive
-    @achieved = true if can_be_archived?
+    @archived = true if can_be_archived?
+  end
+
+  def can_be_archived?
+    # should return true if published_date is older than 10 years
+    @published_date < Date.today - 10_000
   end
 end
