4ee428aec9c7344548783cb7f438a8e317d08c46
vikipretium <vighneshpasupathy@gmail.com>
2022-05-31 18:53:52 +0530

    add method music album data

--- options/add_music_album.rb
+++ options/add_music_album.rb
@@ -1,3 +1,22 @@
 require 'json'
 require_relative '../classes/music_album'
 require_relative '../classes/genre'
+
+def music_album_data
+    valid = false
+    print 'Publish Date: '
+    publish_date = gets.chomp
+    on_spotify = ''
+    until valid
+      print 'On Spotify Y/N:'
+      on_spotify = gets.chomp.downcase
+      valid = %w[y n].include?(on_spotify)
+      on_spotify = '' unless valid
+      puts 'Please insert y or n as the on spotify' unless valid
+    end
+    spotify = false
+    spotify = true if on_spotify == 'y'
+    [publish_date, spotify]
+  end
+
+  
