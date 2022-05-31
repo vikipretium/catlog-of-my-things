cd218a75a85e760c6d686b289177827b86af101f
vikipretium <vighneshpasupathy@gmail.com>
2022-05-31 18:59:31 +0530

    add method for load files

--- options/book_handler.rb
+++ options/book_handler.rb
@@ -6,3 +6,12 @@ class BookHandler
       @new_book = nil
       @destructured_array = nil
     end
+
+    def load_file
+        file_to_load = File.read('books.json')
+        JSON.parse(file_to_load)
+      end
+    
+      def load_file_if_it_exist
+        load_file unless File.file?('books.json') == false
+      end
