75bd74806c2627e30ca54d38ecfb4276c5fd805e
Vignesh <vighneshpasupathy@gmail.com>
2022-06-01 18:49:58 +0530

    Update database/schema.sql
    remove unused columns
    
    Co-authored-by: Juliana Ifionu <julieifionu@gmail.com>

--- database/schema.sql
+++ database/schema.sql
@@ -59,17 +59,8 @@ DROP TABLE IF EXISTS games;
 CREATE TABLE games (
     id INT PRIMARY KEY,
     multiplayer VARCHAR(100),
-    last_played_at DATE NOT NULL,
-    genre_id INT NOT NULL,
-    lable_id INT NOT NULL,
-    source_id INT NOT NULL,
-    author_id INT NOT NULL,
-    publish_date DATE NOT NULL,
-    archived BOOLEAN NOT NULL,
-    FOREIGN KEY genre_id REFERENCES genre (id),
-    FOREIGN KEY lable_id REFERENCES lable (id),
-    FOREIGN KEY author_id REFERENCES author (id),
-    FOREIGN KEY source_id REFERENCES source (id)
+    last_played_at DATE,
+    FOREIGN KEY(item_id) REFERENCES items(id)
 );
 
 DROP TABLE IF EXISTS books;
